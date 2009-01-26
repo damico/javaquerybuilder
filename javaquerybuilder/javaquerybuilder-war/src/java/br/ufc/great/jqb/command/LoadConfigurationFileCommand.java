574f335e0bb7179511e569e6ed8dddb07e9e91bf
leoomoreira <leoomoreira@016099d4-e72d-11dd-b00f-9fabb4b2ff13>
2009-01-26 17:13:38 +0000

    update in the configuration module

--- javaquerybuilder/javaquerybuilder-war/src/java/br/ufc/great/jqb/command/LoadConfigurationFileCommand.java
+++ javaquerybuilder/javaquerybuilder-war/src/java/br/ufc/great/jqb/command/LoadConfigurationFileCommand.java
@@ -1,7 +1,14 @@
 package br.ufc.great.jqb.command;
 
+import br.ufc.great.jqb.config.Configuration;
+import br.ufc.great.jqb.config.JavaQueryBuilder;
+import br.ufc.great.jqb.ejb.ConfigurationBean;
+import br.ufc.great.jqb.ejb.ConfigurationRemote;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -11,6 +18,29 @@ import javax.servlet.http.HttpServletResponse;
 public class LoadConfigurationFileCommand implements Command {
 
     public String execute(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        if (session.getAttribute("configurationFile") == null) {
+            ConfigurationRemote configurationRemote = new ConfigurationBean();
+            String xml = "";
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(Configuration.class.getResourceAsStream("config.xml")));
+                String temp = br.readLine();
+                while (temp != null) {
+                    xml += temp;
+                    temp = br.readLine();
+                }
+                br.close();
+                JavaQueryBuilder jqb = configurationRemote.createConfiguration(xml);
+                if (jqb != null)
+                    session.setAttribute("configurationFile", jqb);
+                return "ControllerServlet?command=mountQuery";
+            }
+            catch (Exception e) {
+
+            }
+        }
         return "WEB-INF/jsp/configuration.jsp";
+
     }
+    
 }
